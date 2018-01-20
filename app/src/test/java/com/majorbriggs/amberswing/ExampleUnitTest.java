d2a422ef9dbc4f9bef4b626bdc25ed171a7ab125
u530282 <piotr.baranowski@lhsystems.com>
2018-01-20 22:55:09 +0100

    add dummy test, update gitignore

--- app/src/test/java/com/majorbriggs/amberswing/ExampleUnitTest.java
+++ app/src/test/java/com/majorbriggs/amberswing/ExampleUnitTest.java
@@ -14,4 +14,9 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void dummyTest() throws Exception {
+        assertEquals(1, 1);
+    }
 }
